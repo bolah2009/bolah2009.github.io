70aca608ffede7f5f33f20795743bbdeacfcd268
Bola Ahmed Buari <deploy@travis-ci.org>
2019-11-25 16:23:12 +0000

    Deploy Bola Buari Portfolio to github.com/bolah2009/bolah2009.github.io.git:master

---
+++ precache-manifest.a5cfdf669f84e0d875dba33d92dd5df1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b8bca35f92637222f2",
+    "url": "display.css"
+  },
+  {
+    "revision": "0175b3059d2e3cae293e75f944778309",
+    "url": "index.html"
+  },
+  {
+    "revision": "295292b9b0e495663e0f",
+    "url": "main.bundle.js"
+  },
+  {
+    "revision": "5a37e779a3b65cb94c09",
+    "url": "styles.css"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(r => r)
-      .catch(e => e);
-  });
-}
